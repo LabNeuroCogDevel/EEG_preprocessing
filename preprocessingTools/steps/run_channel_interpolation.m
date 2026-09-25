ac198397a05eabe8695a9213419fd4ee55d43e62
Abigail Beatty <amb895@pitt.edu>
2026-09-25 11:17:46 -0400

    128 separate from 64; removing Flag128

--- preprocessingTools/steps/run_channel_interpolation.m
+++ preprocessingTools/steps/run_channel_interpolation.m
@@ -1,3 +1,5 @@
+% need EEG.origchanlocs b/c this has channel locations and external channels removed- need for determining which channels have been rejected
+
 if Flag128 == 1 % no clue what is going on here using from original pipeline
     nchan = 64;
     ngood = length(EEG.chanlocs);
@@ -6,12 +8,12 @@ if Flag128 == 1 % no clue what is going on here using from original pipeline
     % AND interp any bad channels
     % do this by removing the 4 128weirdos
     % from the already trimmed (no bad channels) in EEG.chanlocs
-    
+
     need_128interp = [2  3  35  36 ];
     % get the names of those to remove
-    n128name = {originalEEG.chanlocs(need_128interp).labels};
+    n128name = {EEG.origchanlocs(need_128interp).labels};
     % should always be {'AF5','AF1','AF2','AF6'} ??
-    
+
     % find where they are in current EEG files (if they haven't already been removed)
     n128here_idx = find(ismember({EEG.chanlocs.labels},n128name));
     % keep those that aren't the ones we matched
@@ -21,12 +23,12 @@ if Flag128 == 1 % no clue what is going on here using from original pipeline
     EEG.chanlocs = EEG.chanlocs(keep_idx);
     EEG.data = EEG.data(keep_idx,:);
     EEG.nbchan = length(keep_idx);
-    
+
     %EEG_i = pop_interp(EEG, interp_ch, 'spherical');
     fprintf('%d channels in orig; want to interpolate %d bad and move %d\n',...
         originalEEG.nbchan, nchan - ngood, length(need_128interp))
-    EEG_i = pop_interp(EEG, originalEEG.chanlocs, 'spherical');
-    
+    EEG_i = pop_interp(EEG, EEG.origchanlocs, 'spherical');
+
     % could swap these channels (they're close, but not the same)
     % BUT WE DONT
     % 128    'AF7' --> 64    'AF5' In this point channel 2
@@ -35,7 +37,7 @@ if Flag128 == 1 % no clue what is going on here using from original pipeline
     % 128    'AF8' --> 64    'AF6' In this point channel 36
     % lines above modify channel information and pocition in data to make
     %  it the same for 64 and 128 cap
-    
+
     % need to do destructive swapping. need a copy
     EEG = EEG_i;
     EEG.chanlocs(2) = EEG_i.chanlocs(3);%EEG.chanlocs(2) must by 'AF1' in 64 cap
@@ -48,7 +50,7 @@ if Flag128 == 1 % no clue what is going on here using from original pipeline
     EEG.data(2,:) = EEG_i.data(3,:);% ALERT ALERT Lines latelly added
     EEG.data(3,:) = EEG_i.data(2,:);% ATERT ALERT Lines latelly added
 else
-    EEG = pop_interp(EEG, originalEEG.chanlocs, 'spherical');
+    EEG = pop_interp(EEG, EEG.origchanlocs, 'spherical');
     EEG = pop_saveset( EEG,'filename', interp_name, ...
         'filepath', outpath.interpolated);
 end
--- preprocessingTools/steps/run_channel_rejection.m
+++ preprocessingTools/steps/run_channel_rejection.m
@@ -1,51 +1,53 @@
-% define channel locations
-
-if strcmp(study, '7T') || strcmp(study,'Habit')
-    % just need to remove external channels
-    EEG = pop_chanedit(EEG, 'lookup', cap_location);
-    
-    % remove external channels
-    EEG = pop_select( EEG,'nochannel',{'EX3' 'EX4' 'EX5' 'EX6' 'EX7' 'EX8' 'EXG1' 'EXG2' 'EXG3' 'EXG4' 'EXG5' 'EXG6' 'EXG7' 'EXG8' 'GSR1' 'GSR2' 'Erg1' 'Erg2' 'Resp' 'Plet' 'Temp' 'FT7' 'FT8' 'TP7' 'TP8' 'TP9' 'TP10'});
-
-elseif strcmp(study,'SPA')
-    % SPA caps
-    % Channel --> Label
-    % AF1 (2) --> AF3
-    % AF5 (3) --> AF7
-    % TP7 --> NA
-    % FT9 (11) --> FT7
-    % AF2 (35) --> AF4
-    % AF6 (36) --> AF8
-    % FT10 (46) --> FT8
-    % I2 (64) --> Iz
-    % TP8 --> NA
-    % Not using:
-    % PO9, PO10, or I1
-
-    % JUST SPA:
-    EEG.chanlocs(2).labels = 'AF3';
-    EEG.chanlocs(3).labels = 'AF7';
-    EEG.chanlocs(11).labels = 'FT7';
-    EEG.chanlocs(35).labels = 'AF4';
-    EEG.chanlocs(36).labels = 'AF8';
-    EEG.chanlocs(46).labels = 'FT8';
-    EEG.chanlocs(64).labels = 'Iz';
-    
-    EEG = pop_chanedit(EEG, 'lookup', cap_location);
-    
-    % remove external channels and non-used channels
-    EEG = pop_select( EEG,'nochannel',{'EX3' 'EX4' 'EX5' 'EX6' 'EX7' 'EX8' 'TP7' 'TP8' 'PO9' 'I1' 'PO10'});
-
-end
+%% define channel locations
 
 % fixing if 128 channel setup
-if size(EEG.data,1) > 100
+if size(EEG.data,1) > 100 && strcmp(study,'7T')
     EEG = pop_select( EEG,'channel',commonPlus);
     EEG = pop_chanedit(EEG, 'load', {correction_cap_location 'filetype' 'autodetect'});
     % 128    'AF8' --> 64    'AF6'
     % 128    'AF7' --> 64    'AF5'
     % 128    'AF4' --> 64    'AF2'
     % 128    'AF3' --> 64    'AF1'
+
+else % less than 100 channels
+    % Define cap layout for different studies
+    if strcmp(study, '7T') || strcmp(study,'Habit')
+        % just need to remove external channels
+        EEG = pop_chanedit(EEG, 'lookup', cap_location);
+       
+        % remove external channels
+        EEG = pop_select( EEG,'nochannel',{'EX3' 'EX4' 'EX5' 'EX6' 'EX7' 'EX8' 'EXG1' 'EXG2' 'EXG3' 'EXG4' 'EXG5' 'EXG6' 'EXG7' 'EXG8' 'GSR1' 'GSR2' 'Erg1' 'Erg2' 'Resp' 'Plet' 'Temp' 'FT7' 'FT8' 'TP7' 'TP8' 'TP9' 'TP10'});
+
+    elseif strcmp(study,'SPA')
+        % SPA caps
+        % Channel --> Label
+        % AF1 (2) --> AF3
+        % AF5 (3) --> AF7
+        % TP7 --> NA
+        % FT9 (11) --> FT7
+        % AF2 (35) --> AF4
+        % AF6 (36) --> AF8
+        % FT10 (46) --> FT8
+        % I2 (64) --> Iz
+        % TP8 --> NA
+        % Not using:
+        % PO9, PO10, or I1
+        
+        % JUST SPA:
+        EEG.chanlocs(2).labels = 'AF3';
+        EEG.chanlocs(3).labels = 'AF7';
+        EEG.chanlocs(11).labels = 'FT7';
+        EEG.chanlocs(35).labels = 'AF4';
+        EEG.chanlocs(36).labels = 'AF8';
+        EEG.chanlocs(46).labels = 'FT8';
+        EEG.chanlocs(64).labels = 'Iz';
+
+        EEG = pop_chanedit(EEG, 'lookup', cap_location);
+
+        % remove external channels and non-used channels
+        EEG = pop_select( EEG,'nochannel',{'EX3' 'EX4' 'EX5' 'EX6' 'EX7' 'EX8' 'TP7' 'TP8' 'PO9' 'I1' 'PO10'});
+    end
+    
 end
 
 %% Bad Channel Rejection
@@ -58,7 +60,8 @@ end
 % maybe use clean_rawdata but need to tweak from original pipeline
 
 % originalEEG has channel locations and external channels removed- need for determining which channels have been rejected
-originalEEG = EEG;
+% originalEEG = EEG;
+EEG.origchanlocs = EEG.chanlocs;
 
 EEG = clean_rawdata(EEG, 8, -1, 0.7, 5, 15, 0.3);
 % clean_rawdata inputs:
@@ -67,12 +70,6 @@ EEG = clean_rawdata(EEG, 8, -1, 0.7, 5, 15, 0.3);
 % arg_highpass = -1 : already highpassed data do not need to do again
 % LOOK AT OTHER PARAMETERS
 
-%change setname
-EEG = pop_editset(EEG,'setname', chrm_name);
-
-EEG = pop_saveset( EEG,'filename', chrm_name, ...
-    'filepath', outpath.channels_rejected);
-
 % what is clean_channel_mask???
 if ~any(find(cellfun (@any,regexpi (fieldnames(EEG.etc), 'clean_channel_mask'))))
     EEG.etc.clean_channel_mask=42;
@@ -80,7 +77,7 @@ end
 
 %save the channels that were rejected in a variable
 channels_removed{1} = chrm_name; %setname
-channels_removed{2} = setdiff({originalEEG.chanlocs.labels},{EEG.chanlocs.labels}, 'stable');
+channels_removed{2} = setdiff({EEG.origchanlocs.labels},{EEG.chanlocs.labels}, 'stable');
 channels_removed{3} = find(EEG.etc.clean_channel_mask==0);
 
 %also save the channels that were rejected in the EEG struct
@@ -95,3 +92,9 @@ data_removed{3} = length(find(EEG.etc.clean_sample_mask==0))/length(EEG.etc.clea
 %also save the data that were rejected in the EEG struct
 EEG.data_rj    = data_removed{2};
 EEG.data_rj_nr = data_removed{3};
+
+%change setname
+EEG = pop_editset(EEG,'setname', chrm_name);
+% save with all fields added
+EEG = pop_saveset( EEG,'filename', chrm_name, ...
+    'filepath', outpath.channels_rejected);
